f5b6e9130b4684668d6012e7e79703dae78b3dda
Ara Yeressian <ayeressian2@gmail.com>
2015-10-07 15:41:04 +0400

    new arch

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 'use strict';
 
-var bbComponent = require('bb-component');
-var constValues = bbComponent.constValues;
-var ComponentResult = bbComponent.ComponentResult;
-var Component = bbComponent.Component;
+let bbComponent = require('bb-component'),
+    constValues = bbComponent.constValues,
+    ComponentResult = bbComponent.ComponentResult,
+    Component = bbComponent.Component;
 
-function ImageFinder(controllerCallBacks, dataStore) {
-	Component.call(this, controllerCallBacks, dataStore, [constValues.componentOutputTypes.keywords]);
-	this._controllerCallBacks = controllerCallBacks;
+ImageFinder.prototype.execute = function () {
+    this._controllerCallBacks.providerRequest(constValues.providerTypes.flickr);
+};
+
+class ImageFinder extends Component {
+    constructor(controllerCallBacks) {
+        super(controllerCallBacks, [constValues.componentOutputTypes.keywords]);
+    }
+
+    _executeIfReady() {
+        if(this._flickrProvider && this._keywords) {
+            flickrProvider.photos.search({
+                text: this._keywords[0],
+                page: 1,
+                per_page: 500,
+                extras: ['url_c']
+            }, (err, result) => {
+                if (err) {
+                    this._controllerCallBacks.error(err);
+                    return;
+                }
+                this._controllerCallBacks.finish(new ComponentResult(constValues.componentOutputTypes.imageURL, result.photos.photo[0].url_c));
+            });
+        }
+    }
+
+    setProvider(flickrProvider) {
+        this._flickrProvider = flickrProvider;
+        _executeIfReady();
+    }
+
+    dataInput(keywords) {
+        this._keywords = keywords;
+        _executeIfReady();
+    }
+
+    execute() {
+        this._controllerCallBacks.providerRequest(constValues.providerTypes.flickr);
+    }
 }
-
-ImageFinder.prototype = Object.create(Component.prototype);
-
-ImageFinder.prototype.setProvider = function(flickrProvider) {
-	var self = this;
-	flickrProvider.photos.search({
-		text : this._inputs[constValues.componentOutputTypes.keywords][0],
-		page : 1,
-		per_page : 500,
-		extras : [ 'url_c' ]
-	}, function(err, result) {
-		if (err) {
-			self._controllerCallBacks.error(err);
-			return;
-		}
-		self._controllerCallBacks.finish(new ComponentResult(constValues.componentOutputTypes.imageURL, result.photos.photo[0].url_c));
-	});
-};
-
-ImageFinder.prototype.execute = function() {
-	this._controllerCallBacks.providerRequest(constValues.providerTypes.flickr);
-};
 
 module.exports = ImageFinder;
